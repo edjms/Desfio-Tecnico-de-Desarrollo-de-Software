2d82859f3ce17854230a53758e756f6e70a064fd
Esteban de Jesus Mazo Serna <Esteban.Mazo@AXITYCO.LOCAL>
2024-07-15 12:09:18 -0500

    Se agregan directorios del proyecto y se crea proyecto con Spring Boot

---
+++ Backend/TechnicalTestAlianza/src/main/java/co/com/technical_test_alianza/TechnicalTestAlianzaApplication.java
@@ -0,0 +1,13 @@
+package co.com.technical_test_alianza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestAlianzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestAlianzaApplication.class, args);
+	}
+
+}
---
+++ Backend/TechnicalTestAlianza/src/test/java/co/com/technical_test_alianza/TechnicalTestAlianzaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.technical_test_alianza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestAlianzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
